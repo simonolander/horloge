9787cccb2ec33576a029882289ff4da7085c41d8
Simon Olander Sahlén <simon.olander-sahlen@regionstockholm.se>
2023-12-19 14:36:23 +0100

    Colors

--- app/src/main/java/org/simonolander/horloge/ui/theme/Color.kt
+++ app/src/main/java/org/simonolander/horloge/ui/theme/Color.kt
@@ -1,14 +1,15 @@
 package org.simonolander.horloge.ui.theme
 
 import androidx.compose.ui.graphics.Color
-import org.simonolander.horloge.R
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFFF0000)
+val PurpleGrey80 = Color(0xFFFF0000)
+val Pink80 = Color(0xFFFF0000)
 
 val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PurpleGrey40 = Color(0xFFFF0000)
+val Pink40 = Color(0xFFFF0000)
 
-val HorlogeOrange = Color(R.color.orange_horloge)
+val OrangeDark = Color(0xFFDB900F)
+val OrangeHorloge = Color(0xFFFEA711)
+val OrangeNeutral = Color(0xFFF6EDE4)
